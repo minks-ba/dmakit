b9c1057ea4703823bdc3150776aaa67d04725d2e
BARRIE Mohamed <OBH706@btl.bollore.com>
2022-08-01 18:18:34 +0000

    update

--- JS/index.js
+++ JS/index.js
@@ -29,8 +29,34 @@ function search() {
 };
 
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 
+    navigation: {
+        nextEl: '.swiper-button-next'.style,
+        prevEl: '.swiper-button-prev',
+    },
 
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      }
+    }
+});
 
 
  
