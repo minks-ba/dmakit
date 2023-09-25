f4f65f44a57e7e1be16072285e8125f79ea2a5e6
Mohamed BARRIE (AGL) <OBH706@btl.bollore.com>
2023-09-25 12:26:09 +0000

    update

--- JS/product-des.js
+++ JS/product-des.js
@@ -27,6 +27,16 @@ function display() {
   }
 };
 
+// var swiper = new Swiper(".mySwiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     renderBullet: function (index, className) {
+//       return '<span class="' + className + '">' + (index + 1) + "</span>";
+//     },
+//   },
+// });
+
 //  Product Details - Image Slider 
 
 var swiper = new Swiper(".mySwiper", {
